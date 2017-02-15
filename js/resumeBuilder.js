1e7e5046ef3c1f3b636b98c3b71a676824ef7b3f
DESKTOP-1BBR5UQ\Al Faris <ihhkamu1@gmail.com>
2017-02-15 19:59:15 +0700

    Add work object and education .

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,7 @@
 var fullName = "MuhamMAD AL FARIS";
 var role = "Junior Programmer";
-var bio = {
+
+	var bio = {
 	"name" : "Muhammad Al Faris",
 	"role" : role,
 	"contact" : {
@@ -11,10 +12,19 @@ var bio = {
 	"picture_url" : "images/me.jpg",
 	"welcome_message" : "I just newbie !",
 	"skill" : ["C#, Javascript, Java, some Basic HTML & CSS, XML"]
-};
+	}
+	bio.experience = "IT Support - 2 Years";
 
-bio.experience = "IT Support - 2 Years";
+var work = {};
+work.employer = "CV Nagatech System Integrator";
+work.yearsWorked = -1;
+work.cityEmployer = "Bandung";
+work.position = "Junior Programmer";
 
+var education = {};
+education["nameLastSchool"] = "SMK Medikacom Bandung";
+education["yearsKeluar"] = 2009;
+education["Schoolcity"] = "Bandung";
 //SplitName
 var splitName = fullName.split(" ");
 for (var i = 0; i < splitName.length; i++) {
@@ -23,8 +33,10 @@ for (var i = 0; i < splitName.length; i++) {
 fullName = splitName.join(" ");
 
 //Replace Data
-var formattedName = HTMLheaderName.replace("%data%", bio.experience);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 var array = [formattedName, formattedRole];
 
 $("#header").append(array);
+$("#main").append(work["position"]);
+$("#main").append(education.nameLastSchool);
