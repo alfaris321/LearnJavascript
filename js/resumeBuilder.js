32ee8838a21f1940de20d79985776586a266a19d
DESKTOP-1BBR5UQ\Al Faris <ihhkamu1@gmail.com>
2017-02-15 13:35:09 +0700

    Use .replace('value1','value2');

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-var awesomeThought = "I am Muhammad Al Faris , I Am AWESOME!"
+var awesomeThought = "I am Muhammad Al Faris , I Am AWESOME!";
+var funThought = awesomeThought.replace("AWESOME","FUN");
+
+console.log(awesomeThought);
+console.log(funThought);
