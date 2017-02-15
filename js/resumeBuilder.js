c48e49b1d7dc0f56b8b7ff3637ece27931fc91cd
DESKTOP-1BBR5UQ\Al Faris <ihhkamu1@gmail.com>
2017-02-15 19:34:22 +0700

    Learn how 'Object' work in Javascript...

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,19 @@
 var fullName = "MuhamMAD AL FARIS";
 var role = "Junior Programmer";
+var bio = {
+	"name" : "Muhammad Al Faris",
+	"role" : role,
+	"contact" : {
+		"twitter" : "@MuhAl_Faris",
+		"facebook": "Muhammad Al Faris",
+		"WhatsApp" : 081910373275
+	},
+	"picture_url" : "images/me.jpg",
+	"welcome_message" : "I just newbie !",
+	"skill" : ["C#, Javascript, Java, some Basic HTML & CSS, XML"]
+};
+
+bio.experience = "IT Support - 2 Years";
 
 //SplitName
 var splitName = fullName.split(" ");
@@ -9,8 +23,8 @@ for (var i = 0; i < splitName.length; i++) {
 fullName = splitName.join(" ");
 
 //Replace Data
-var formattedName = HTMLheaderName.replace("%data%", fullName);
+var formattedName = HTMLheaderName.replace("%data%", bio.experience);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 var array = [formattedName, formattedRole];
 
-$("#header").prepend(array);
+$("#header").append(array);
