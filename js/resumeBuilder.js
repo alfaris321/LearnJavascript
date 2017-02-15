ee6171f5665af454c67c6561dc2d4452989a0b90
DESKTOP-1BBR5UQ\Al Faris <ihhkamu1@gmail.com>
2017-02-15 23:07:19 +0700

    Learn for - each , for - in , and add some object to Code.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,42 +1,82 @@
-var fullName = "MuhamMAD AL FARIS";
-var role = "Junior Programmer";
-
-	var bio = {
-	"name" : "Muhammad Al Faris",
-	"role" : role,
+var bio = {
+	"fullname" : {
+		"firstName" : "Muhammad",
+		"lastname" : "Al Faris"
+	} ,
+	"role" : "Junior Programmer",
 	"contact" : {
 		"twitter" : "@MuhAl_Faris",
 		"facebook": "Muhammad Al Faris",
-		"WhatsApp" : 081910373275
+		"WhatsApp" : "081910373275"
 	},
 	"picture_url" : "images/me.jpg",
 	"welcome_message" : "I just newbie !",
 	"skill" : ["C#, Javascript, Java, some Basic HTML & CSS, XML"]
-	}
-	bio.experience = "IT Support - 2 Years";
+	};
 
-var work = {};
-work.employer = "CV Nagatech System Integrator";
-work.yearsWorked = -1;
-work.cityEmployer = "Bandung";
-work.position = "Junior Programmer";
+var work = {
+	"jobs" : 
+	[ 
+	{
+		"employer" : "PT LUCAS DJAJA",
+		"position" : "Operator Produksi",
+		"cityEmployer" : "Bandung",
+		"yearsWorked" : "2month"
+	},
+	{
+		"employer" : "CV Nagatech System Integrator",
+		"position" : "Junior Programmer",
+		"cityEmployer" : "Bandung",
+		"yearsWorked" : "2month"
+	}
+ 	]
+};
 
 var education = {};
 education["nameLastSchool"] = "SMK Medikacom Bandung";
-education["yearsKeluar"] = 2009;
+education["yearsKeluar"] = "2009";
 education["Schoolcity"] = "Bandung";
-//SplitName
-var splitName = fullName.split(" ");
-for (var i = 0; i < splitName.length; i++) {
-	splitName[i] = splitName[i].slice(0,1).toUpperCase() + splitName[i].slice(1).toLowerCase();
-}
-fullName = splitName.join(" ");
+
+var cv = {
+	"dataDiri" : {
+		bio
+	},
+	"experience" : {
+		work
+	},
+	"education" : {
+		education
+	}
+};
+
+
 
 //Replace Data
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedName = HTMLheaderName.replace("%data%", bio.fullname.firstName + bio.fullname.lastname);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var array = [formattedName, formattedRole];
 
 $("#header").append(array);
-$("#main").append(work["position"]);
-$("#main").append(education.nameLastSchool);
+
+if (bio.skill.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	var formattedSkill;
+	for (var i = 0; i < bio.skill.length; i++) {
+		formattedSkill = HTMLskills.replace("%data%", bio.skill[i])
+		$("#skills").append(formattedSkill);
+	}
+};
+
+if (work.jobs.length > 0) {
+	$("#header").append(HTMLworkStart);
+
+	
+	for (jobs in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedJobs = HTMLworkEmployer.replace("%data%",work.jobs[jobs].employer);
+		var formattedPosition = HTMLworkTitle.replace("%data%",work.jobs[jobs].position);
+		var formattedEmployerTitle = formattedJobs + formattedPosition;
+
+		$(".work-entry:last").append(formattedEmployerTitle);
+	}
+};
