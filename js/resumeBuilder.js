11fc4ae6a1685eeeba34f58b0080d62b77a8bc9c
DESKTOP-1BBR5UQ\Al Faris <ihhkamu1@gmail.com>
2017-02-15 18:58:55 +0700

    manipulation String Eps.1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,14 @@
-var formattedName = HTMLheaderName.replace("%data%", "Muhammad Al Faris");
+var fullName = "MuhamMAD AL FARIS";
 
+var splitName = fullName.split(" ");
+splitName[0] = splitName[0].slice(0,1).toUpperCase() + splitName[0].slice(1).toLowerCase();
+splitName[1] = splitName[1].slice(0,1).toUpperCase() + splitName[1].slice(1).toLowerCase();
+splitName[2] = splitName[2].slice(0,1).toUpperCase() + splitName[2].slice(1).toLowerCase();
+fullName = splitName.join(" ");
+var formattedName = HTMLheaderName.replace("%data%", fullName);
+ 
 var role = "Junior Programmer";
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+var array = [formattedName, formattedRole];
 
-$("#header").prepend(formattedName);
-$("#header").prepend(formattedRole);
+$("#header").prepend(array);
